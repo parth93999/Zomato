2f32fe09da28a1fc47c0edbdac80abc083f520de
parth973795 <parth973795@gmail.com>
2026-07-13 14:53:03 +0530

    Harshit

---
+++ Backend/Confiq/db.js
@@ -0,0 +1,19 @@
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: 'tokaido.proxy.rlwy.net',
+  port: 43983,
+  user: 'root',
+  password: 'sElHAkatAmocdzMFZIccZHKASkWHWymt',
+  database: 'railway'
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting to database:', err);
+    return;
+  }
+  console.log('Connected to database successfully');
+});
+
+module.exports = connection;
